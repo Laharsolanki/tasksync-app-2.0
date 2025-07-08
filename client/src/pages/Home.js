9f125a16abd99e883c994a29d177d92d037afecf
Lahar_07 <laharsolanki1107@gmail.com>
2025-07-08 08:12:58 +0530

    Day 8 : Connected frontend with deployed backend

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,7 +6,7 @@ function Home() {
   const [task, setTask] = useState("");
   const [tasks, setTasks] = useState([]);
 
-  const API_BASE = "https://adaptable-gentleness-production.up.railway.app";
+  const API_BASE = "https://adaptable-gentleness-production.up.railway.app/api/tasks";
 
   // Fetch tasks from server
   useEffect(() => {
