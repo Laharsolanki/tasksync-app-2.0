a91233023ab4fb982fa1f646631f4bf21060ad0d
Lahar_07 <laharsolanki1107@gmail.com>
2025-09-08 10:40:01 +0530

    Solved errors

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
-
 
 export const getTasks = () => axios.get(`${API_URL}/tasks`);
 export const createTask = (payload) => axios.post(`${API_URL}/tasks`, payload);
